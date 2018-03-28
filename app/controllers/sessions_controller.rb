73f155a637449bd0a8e128f1fd05ec2d2c1ccc96
Duong Nguyen <nguyenduonghd96@gmail.com>
2018-03-28 13:13:03 +0700

    Chuong 9 Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == Settings.session_ctl_number ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = t ".login_flash"
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
